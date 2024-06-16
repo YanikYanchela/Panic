ccac96ba91d7b62100b616afb5ef72a4c5877656
YanikYanchela <yanik-1993@rambler.ru>
2024-06-16 17:47:26 +0300

    Initial Commit

---
+++ Panic/PanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanicApp.swift
+//  Panic
+//
+//  Created by Дмитрий Яновский on 16.06.24.
+//
+
+import SwiftUI
+
+@main
+struct PanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
